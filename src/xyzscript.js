196e5a641648bb389a4ca48385cdf5128f6f746b
XYZ <xyzscer@outlook.com>
2022-12-12 22:23:48 +0800

    Update xyzscript.js

--- src/xyzscript.js
+++ src/xyzscript.js
@@ -1,56 +1,29 @@
-function Script(code) {
+import { open, writeFile } from "fs"
+function Script(path = undefined) {
   // 如果用户没有使用“new”关键字，就返回new Script
   if (!(this instanceof Script)) {
-    return new Script(code);
+    return new Script(path);
   }
-}
-Script.prototype.toJavaScript = function (input) {
-  function tokenizer(input) {
+  this.havefile = !!path;
+  Script.prototype.toJavaScript = function (input) {
+    function tokenizer(input) {
 
-    // `current` 变量就像一个指光标一样让我们可以在代码中追踪我们的位置
-    let current = 0;
+      // `current` 变量就像一个指光标一样让我们可以在代码中追踪我们的位置
+      let current = 0;
 
-    // `tokens` 数组用来存我们的标记
-    let tokens = [];
+      // `tokens` 数组用来存我们的标记
+      let tokens = [];
 
-    // 首先创建一个`while`循环，在循环里面我们可以将`current`变量增加为我们想要的值
-    // 做这一步的原因是因为我们想要在一个循环里多次增加`current`变量的值，因为我们的标记的长度不确定
-    while (current < input.length) {
-      let char = input[current];
+      // 首先创建一个`while`循环，在循环里面我们可以将`current`变量增加为我们想要的值
+      // 做这一步的原因是因为我们想要在一个循环里多次增加`current`变量的值，因为我们的标记的长度不确定
+      while (current < input.length) {
+        let char = input[current];
+        let hang = 1;
 
-      if (char === '有') {
-        tokens.push({
-          type: '标签',
-          value: '有',
-        });
-        // `current`自增
-        current++;
-
-        // 然后继续进入下一次循环。
-        continue;
-      }
-
-      if (char === '值') {
-        current++;
-        if (input[current] === '为') {
-          tokens.push({
-            type: '符号',
-            value: '值为',
-          });
-          // `current`自增
-          current++;
-
-          // 然后继续进入下一次循环。
-          continue;
-        }
-      }
-
-      if (char === '方') {
-        current++;
-        if (input[current] === '法') {
+        if (char === '有') {
           tokens.push({
             type: '标签',
-            value: '方法',
+            value: '有',
           });
           // `current`自增
           current++;
@@ -58,123 +31,195 @@ Script.prototype.toJavaScript = function (input) {
           // 然后继续进入下一次循环。
           continue;
         }
-      }
 
-      let WHITESPACE_AND_OTHER = /[\s，]/;
-      if (WHITESPACE_AND_OTHER.test(char)) {
-        current++;
-        continue;
-      }
+        if (char === '值') {
+          current++;
+          if (input[current] === '为') {
+            tokens.push({
+              type: '符号',
+              value: '值为',
+            });
+            // `current`自增
+            current++;
 
-      let NUMBERS = /[0-9]/;
-      if (NUMBERS.test(char)) {
-
-        // 我们将创建一个`value`字符串，并把字符推送给他
-        let value = '';
-
-        // 然后我们将遍历序列中的每个字符，直到遇到一个不是数字的字符
-        // 将每个作为数字的字符推到我们的 `value` 并随着我们去增加 `current`
-        // 这样我们就能拿到一个完整的数字字符串，例如上面的 123 和 456，而不是单独的 1 2 3 4 5 6
-        while (NUMBERS.test(char)) {
-          value += char;
-          char = input[++current];
+            // 然后继续进入下一次循环。
+            continue;
+          }
         }
 
-        // 接着我们把数字放到标记数组中，用数字类型来描述区分它
-        tokens.push({ type: '数字', value });
+        if (char === '方') {
+          current++;
+          if (input[current] === '法') {
+            tokens.push({
+              type: '标签',
+              value: '方法',
+            });
+            // `current`自增
+            current++;
 
-        // 继续外层的下一次循环
-        continue;
-      }
-
-      if (char === '“') {
-        // 保留一个 `value` 变量来构建我们的字符串标记。
-        let value = '';
-
-        // 我们将跳过编辑中开头的双引号
-        char = input[++current];
-
-        // 然后我们将遍历每个字符，直到我们到达另一个双引号
-        while (char !== '”') {
-          value += char;
-          char = input[++current];
+            // 然后继续进入下一次循环。
+            continue;
+          }
         }
 
-        // 跳过相对应闭合的双引号.
-        char = input[++current];
-
-        // 把我们的字符串标记添加到标记数组中
-        tokens.push({ type: '字符串', value });
-
-        continue;
-      }
-
-      let LETTERS = /[\u4E00-\u9FA5a-z_$-]/i;
-      if (LETTERS.test(char)) {
-        let value = '';
-
-        // 同样，我们遍历所有，并将它们完整的存到`value`变量中
-        while (LETTERS.test(char)) {
-          value += char;
-          char = input[++current];
-          //console.log(value)
+        let WHITESPACE_AND_OTHER = /[\s，]/;
+        if (WHITESPACE_AND_OTHER.test(char)) {
+          /*if (/[\r\n]/.test(char)) {
+            hang+=1
+          }*/
+          current++;
+          continue;
         }
 
-        // 并把这种名称类型的标记存到标记数组中，继续循环
-        tokens.push({ type: '名字', value });
-        continue;
-      }
+        let NUMBERS = /[0-9]/;
+        if (NUMBERS.test(char)) {
 
-      let JUHAO = "。";
-      if (char === JUHAO) {
-        //input[current].replace(JUHAO,";");// 将句号替换成分号
-        tokens.push({ type: '符号', value: ";" });
-        current++;
-        continue;
-      }
+          // 我们将创建一个`value`字符串，并把字符推送给他
+          let value = '';
 
-      let SIZEYUNSUAN = /[\+\*/\-]/;
-      if (SIZEYUNSUAN.test(char)) {
-        let value = char;
-        tokens.push({ type: '符号', value });
-        current++;
-        continue;
-      }
+          // 然后我们将遍历序列中的每个字符，直到遇到一个不是数字的字符
+          // 将每个作为数字的字符推到我们的 `value` 并随着我们去增加 `current`
+          // 这样我们就能拿到一个完整的数字字符串，例如上面的 123 和 456，而不是单独的 1 2 3 4 5 6
+          while (NUMBERS.test(char)) {
+            value += char;
+            char = input[++current];
+          }
 
-      throw new SyntaxError('I dont know what this character is: ' + char);
+          // 接着我们把数字放到标记数组中，用数字类型来描述区分它
+          tokens.push({ type: '数字', value });
+
+          // 继续外层的下一次循环
+          continue;
+        }
+
+        if (char === '“') {
+          // 保留一个 `value` 变量来构建我们的字符串标记。
+          let value = '';
+
+          // 我们将跳过编辑中开头的双引号
+          char = input[++current];
+
+          // 然后我们将遍历每个字符，直到我们到达另一个双引号
+          while (char !== '”') {
+            value += char;
+            char = input[++current];
+          }
+
+          // 跳过相对应闭合的双引号.
+          char = input[++current];
+
+          // 把我们的字符串标记添加到标记数组中
+          tokens.push({ type: '字符串', value });
+
+          continue;
+        }
+
+        let JUHAO = "。";
+        if (char === JUHAO) {
+          //input[current].replace(JUHAO,";");// 将句号替换成分号
+          tokens.push({ type: '符号', value: ";" });
+          current++;
+          continue;
+        }
+
+        let SIZEYUNSUAN = /[\+\*/\-]/;
+        if (SIZEYUNSUAN.test(char)) {
+          let value = char;
+          tokens.push({ type: '符号', value });
+          current++;
+          continue;
+        }
+
+        if (char === "的") {
+          tokens.push({ type: '符号', value: "." })
+          current++;
+          continue;
+        }
+
+        if (char === "空") {
+          tokens.push({ type: '类型', value: "空" })
+          current++;
+          continue;
+        }
+
+        if (char === "{") {
+          let value = "";
+          char = input[++current];
+          while (char !== "}") {
+            value += char;
+            char = input[++current];
+          }
+          char = input[++current];
+
+          // 把我们的字符串标记添加到标记数组中
+          tokens.push({ type: '对象的内容', value });
+
+          continue;
+        }
+
+        let LETTERS = /[\u4E00-\u9FA5a-z_$-]/i;
+        if (LETTERS.test(char)) {
+          let value = '';
+
+          // 同样，我们遍历所有，并将它们完整的存到`value`变量中
+          while (LETTERS.test(char)) {
+            value += char;
+            char = input[++current];
+            //console.log(value)
+          }
+
+          // 并把这种名称类型的标记存到标记数组中，继续循环
+          if (tokens[tokens.length - 1].value !== "值为") {
+            tokens.push({ type: '名字', value });
+          } else {
+            throw new SyntaxError("请记得把字符串加上双引号！")
+          }
+          continue;
+        }
+
+        throw new SyntaxError(`我不知道这个字符是啥意思：${char}。位于第${current + 1}个字符。`);
+      }
+      //console.log(tokens);
+      return tokens;
     }
-    //console.log(tokens);
-    return tokens;
-  }
-  function _toJavaScript(tokens) {
-    //console.log(tokens[0].type)
-    let i = -1;
-    while (i < tokens.length - 1) {
-      i++;
-      // 判断类型，进行替换
-      switch (tokens[i].type) {
-        case "标签": {
-          tokens[i].value = tokens[i].value.replaceAll("有", "var");
-          continue;
+    function _toJavaScript(tokens) {
+      //console.log(tokens[0].type)
+      let i = -1;
+      while (i < tokens.length - 1) {
+        i++;
+        // 判断类型，进行替换
+        switch (tokens[i].type) {
+          case "标签": {
+            tokens[i].value = tokens[i].value.replaceAll("有", "var");
+            continue;
+          }
+          //break;
+          case "符号": {
+            tokens[i].value = tokens[i].value.replaceAll("值为", "=");
+            continue;
+          };
+          //break;
+          case "字符串": {
+            tokens[i].value = `"${tokens[i].value}"`;
+            continue;
+          }
+          case "类型": {
+            tokens[i].value = tokens[i].value.replaceAll("空", "null");
+            continue;
+          }
+          //break;
         }
-        //break;
-        case "符号": {
-          tokens[i].value = tokens[i].value.replaceAll("值为", "=");
-          continue;
-        };
-        //break;
-        case "字符串": {
-          tokens[i].value = `"${tokens[i].value}"`;
-          continue;
-        }
-        //break;
       }
+      console.log(tokens);
+      return tokens.map((x) => { return x.value }).join(" ");
     }
-    console.log(tokens);
-    return tokens.map((x) => { return x.value }).join(" ");
+    //console.log(_toJavaScript(tokenizer('有xyz，值为 “1235”')))
+    if (this.havefile) {
+
+    } else {
+      return _toJavaScript(tokenizer(input))
+    }
   }
-  //console.log(_toJavaScript(tokenizer('有xyz，值为 “1235”')))
-  return _toJavaScript(tokenizer(input))
 }
 //const es = new Script();
 //es.toJavaScript(``);
