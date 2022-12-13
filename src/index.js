f5e1b1c1b6477bbdc5e8ac21369c102ae87dd34d
XYZ <xyzscer@outlook.com>
2022-12-13 22:00:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import Sc from "./xyzscript.js"
 let x=new Sc()
-console.log(x.toJavaScript(`
+/*console.log(x.toJavaScript(`
 有我的第一个变量，值为“hello,world!”。
-有a，值为空。`))
+有a，值为(1+1)*2。
+打印(1+2)`))*/
+eval(x.toJavaScript(`
+有b，值为“hello,world!”。
+有a，值为(1+1)*2。
+打印(a*2)。
+打印(b 的 replaceAll(“!”、“!!!”))。
+打印(b 的 replace 的 toString())`))
